2ca27ccda2ed14e22e800748e78c44fc477d6652
Maryam6713 <maryammalik6713@gmail.com>
2024-06-27 13:51:03 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var sentence = "Hi I am maryam amir."
+console.log(sentence.charAt(5));
+
+var num = 70.50
+console.log(Math.round(num));
+
+var num1 = 80.70
+console.log(Math.floor(num1));
+
+var num1 = 80.70
+console.log(Math.ceil(num1));
+
+var bigDecimal = Math.random();
+ var improvedNum = (bigDecimal * 6) + 1;
+ console.log(improvedNum);
+
+ var bigDecimal = Math.random();
+ var improvedNum = bigDecimal + 0;
+ console.log(improvedNum);
+
+ var num = "20"
+ var floatnum = parseFloat(num)
+ console.log(num);
+ console.log(typeof(floatnum));
+
+ var num = "30"
+ var floatnum = parseInt(num)
+ console.log(num);
+ console.log(typeof(floatnum));
+
+ var num = "maryam"
+ var floatnum = Number(num)
+ console.log(num);
+ console.log(typeof(floatnum));
+
+
+ var num = 6713
+ var floatnum = toString(num)
+ console.log(num);
+ console.log(typeof(floatnum));
+
+ var num = 89.999999
+ console.log(num.toFixed());
+
+ var num = 89.50000000
+ console.log(num.toFixed(2));
